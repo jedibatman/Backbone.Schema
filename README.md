dbefe0884314d497ffaa1debc06fe46ac94a8c89
Dmytro Nemoga <Dmytro.Nemoga@gmail.com>
2013-01-30 21:40:26 +0200

    Renaming schema to model

--- README.md
+++ README.md
@@ -14,65 +14,65 @@ The plugin is for automatic converting model's attributes to the specific type.
 ## Getting Started
 ### Create model
 ```js
-var schema = new Backbone.Model();
+var model = new Backbone.Model();
 ```
 
 ### Define properties
 #### model.defineProperty(attribute, type, [options])
 ##### String type
 ```js
-schema.defineProperty('string', 'string');
+model.defineProperty('string', 'string');
 
-schema.set('string', 999999.99); // schema.attributes.string -> "999999.99"
-schema.get('string'); // "999999.99"
+model.set('string', 999999.99); // model.attributes.string -> "999999.99"
+model.get('string'); // "999999.99"
 ```
 
 ##### Number type
 ```js
-schema.defineProperty('number', 'number');
+model.defineProperty('number', 'number');
 
-schema.set('number', '999,999.99'); // schema.attributes.number -> 999999.99
-schema.get('number'); // "999,999.99"
+model.set('number', '999,999.99'); // model.attributes.number -> 999999.99
+model.get('number'); // "999,999.99"
 ```
 
 ##### Boolean type
 ```js
-schema.defineProperty('boolean', 'boolean');
+model.defineProperty('boolean', 'boolean');
 
-schema.set('boolean', 'true'); // schema.attributes.boolean -> true
-schema.get('boolean'); // true
+model.set('boolean', 'true'); // model.attributes.boolean -> true
+model.get('boolean'); // true
 ```
 
 ##### Date type
 ```js
-schema.defineProperty('date', 'date');
+model.defineProperty('date', 'date');
 
-schema.set('date', '12/31/2012'); // schema.attributes.date -> 1356904800000
-schema.get('date'); // "12/31/2012"
+model.set('date', '12/31/2012'); // model.attributes.date -> 1356904800000
+model.get('date'); // "12/31/2012"
 ```
 
 ##### Text type
 ```js
-schema.defineProperty('text', 'text');
+model.defineProperty('text', 'text');
 
-schema.set('text', '<b>text</b>'); // schema.attributes.text -> "&lt;b&gt;text&lt;&#x2F;b&gt;"
-schema.get('text'); // "<b>text</b>"
+model.set('text', '<b>text</b>'); // model.attributes.text -> "&lt;b&gt;text&lt;&#x2F;b&gt;"
+model.get('text'); // "<b>text</b>"
 ```
 
 ##### Currency type
 ```js
-schema.defineProperty('currency', 'currency');
+model.defineProperty('currency', 'currency');
 
-schema.set('currency', '$999,999.99'); // schema.attributes.currency -> 999999.99
-schema.get('currency'); // "$999,999.99"
+model.set('currency', '$999,999.99'); // model.attributes.currency -> 999999.99
+model.get('currency'); // "$999,999.99"
 ```
 
 ##### Percent type
 ```js
-schema.defineProperty('percent', 'percent');
+model.defineProperty('percent', 'percent');
 
-schema.set('percent', '99.99 %'); // schema.attributes.percent -> 0.9999
-schema.get('percent'); // "99.99 %"
+model.set('percent', '99.99 %'); // model.attributes.percent -> 0.9999
+model.get('percent'); // "99.99 %"
 ```
 
 *All formatted values depends from current [culture](https://github.com/jquery/globalize#culture).*
--- test/backbone/model.js
+++ test/backbone/model.js
@@ -6,7 +6,7 @@ $(function () {
     // PREREQUISITES //
     ///////////////////
 
-    var Schema = Backbone.Model.extend({
+    var Model = Backbone.Model.extend({
         initialize: function () {
             this.defineProperty('string', 'string');
             this.defineProperty('number', 'number');
@@ -24,7 +24,7 @@ $(function () {
 
     module('Backbone.Schema', {
         setup: function () {
-            this.schema = new Schema({
+            this.model = new Model({
                 string: 'string',
                 number: 999999.99,
                 boolean: true,
@@ -41,167 +41,167 @@ $(function () {
     ///////////
 
     test('initial attributes values', function () {
-        strictEqual(this.schema.attributes.string, 'string');
-        strictEqual(this.schema.attributes.number, 999999.99);
-        strictEqual(this.schema.attributes.boolean, true);
-        strictEqual(this.schema.attributes.date, Date.parse('12/31/2012'));
-        strictEqual(this.schema.attributes.text, _.escape('<b>text</b>'));
-        strictEqual(this.schema.attributes.currency, 999999.99);
-        strictEqual(this.schema.attributes.percent, 0.9999);
+        strictEqual(this.model.attributes.string, 'string');
+        strictEqual(this.model.attributes.number, 999999.99);
+        strictEqual(this.model.attributes.boolean, true);
+        strictEqual(this.model.attributes.date, Date.parse('12/31/2012'));
+        strictEqual(this.model.attributes.text, _.escape('<b>text</b>'));
+        strictEqual(this.model.attributes.currency, 999999.99);
+        strictEqual(this.model.attributes.percent, 0.9999);
     });
 
     test('get string property', function () {
-        strictEqual(this.schema.get('string'), 'string');
+        strictEqual(this.model.get('string'), 'string');
     });
 
     test('get number property', function () {
-        strictEqual(this.schema.get('number'), '999,999.99');
+        strictEqual(this.model.get('number'), '999,999.99');
     });
 
     test('get boolean property', function () {
-        strictEqual(this.schema.get('boolean'), true);
+        strictEqual(this.model.get('boolean'), true);
     });
 
     test('get date property', function () {
-        strictEqual(this.schema.get('date'), '12/31/2012');
+        strictEqual(this.model.get('date'), '12/31/2012');
     });
 
     test('get text property', function () {
-        strictEqual(this.schema.get('text'), '<b>text</b>');
+        strictEqual(this.model.get('text'), '<b>text</b>');
     });
 
     test('get currency property', function () {
-        strictEqual(this.schema.get('currency'), '$999,999.99');
+        strictEqual(this.model.get('currency'), '$999,999.99');
     });
 
     test('get percent property', function () {
-        strictEqual(this.schema.get('percent'), '99.99 %');
+        strictEqual(this.model.get('percent'), '99.99 %');
     });
 
     test('set string property', function () {
-        this.schema.set('string', 'string');
-        strictEqual(this.schema.attributes.string, 'string');
+        this.model.set('string', 'string');
+        strictEqual(this.model.attributes.string, 'string');
 
-        this.schema.set('string', 999999.99);
-        strictEqual(this.schema.attributes.string, '999999.99');
+        this.model.set('string', 999999.99);
+        strictEqual(this.model.attributes.string, '999999.99');
 
-        this.schema.set('string', true);
-        strictEqual(this.schema.attributes.string, 'true');
+        this.model.set('string', true);
+        strictEqual(this.model.attributes.string, 'true');
 
-        this.schema.set('string', null);
-        strictEqual(this.schema.attributes.string, null);
+        this.model.set('string', null);
+        strictEqual(this.model.attributes.string, null);
 
-        this.schema.set('string', undefined);
-        strictEqual(this.schema.attributes.string, null);
+        this.model.set('string', undefined);
+        strictEqual(this.model.attributes.string, null);
     });
 
     test('set number property', function () {
-        this.schema.set('number', '999,999.99');
-        strictEqual(this.schema.attributes.number, 999999.99);
+        this.model.set('number', '999,999.99');
+        strictEqual(this.model.attributes.number, 999999.99);
 
-        this.schema.set('number', 999999.99);
-        strictEqual(this.schema.attributes.number, 999999.99);
+        this.model.set('number', 999999.99);
+        strictEqual(this.model.attributes.number, 999999.99);
 
-        this.schema.set('number', true);
-        strictEqual(isNaN(this.schema.attributes.number), true);
+        this.model.set('number', true);
+        strictEqual(isNaN(this.model.attributes.number), true);
 
-        this.schema.set('number', null);
-        strictEqual(this.schema.attributes.number, null);
+        this.model.set('number', null);
+        strictEqual(this.model.attributes.number, null);
 
-        this.schema.set('number', undefined);
-        strictEqual(this.schema.attributes.number, null);
+        this.model.set('number', undefined);
+        strictEqual(this.model.attributes.number, null);
     });
 
     test('set boolean property', function () {
-        this.schema.set('boolean', 'true');
-        strictEqual(this.schema.attributes.boolean, true);
+        this.model.set('boolean', 'true');
+        strictEqual(this.model.attributes.boolean, true);
 
-        this.schema.set('boolean', 999999.99);
-        strictEqual(this.schema.attributes.boolean, true);
+        this.model.set('boolean', 999999.99);
+        strictEqual(this.model.attributes.boolean, true);
 
-        this.schema.set('boolean', true);
-        strictEqual(this.schema.attributes.boolean, true);
+        this.model.set('boolean', true);
+        strictEqual(this.model.attributes.boolean, true);
 
-        this.schema.set('boolean', null);
-        strictEqual(this.schema.attributes.boolean, null);
+        this.model.set('boolean', null);
+        strictEqual(this.model.attributes.boolean, null);
 
-        this.schema.set('boolean', undefined);
-        strictEqual(this.schema.attributes.boolean, null);
+        this.model.set('boolean', undefined);
+        strictEqual(this.model.attributes.boolean, null);
     });
 
     test('set date property', function () {
-        this.schema.set('date', '12/31/2012');
-        strictEqual(this.schema.attributes.date, Date.parse('12/31/2012'));
+        this.model.set('date', '12/31/2012');
+        strictEqual(this.model.attributes.date, Date.parse('12/31/2012'));
 
-        this.schema.set('date', 999999.99);
-        strictEqual(this.schema.attributes.date, 999999);
+        this.model.set('date', 999999.99);
+        strictEqual(this.model.attributes.date, 999999);
 
-        this.schema.set('date', true);
-        strictEqual(this.schema.attributes.date, 1);
+        this.model.set('date', true);
+        strictEqual(this.model.attributes.date, 1);
 
-        this.schema.set('date', null);
-        strictEqual(this.schema.attributes.date, null);
+        this.model.set('date', null);
+        strictEqual(this.model.attributes.date, null);
 
-        this.schema.set('date', undefined);
-        strictEqual(this.schema.attributes.date, null);
+        this.model.set('date', undefined);
+        strictEqual(this.model.attributes.date, null);
     });
 
     test('set text property', function () {
-        this.schema.set('text', '<b>text</b>');
-        strictEqual(this.schema.attributes.text, _.escape('<b>text</b>'));
+        this.model.set('text', '<b>text</b>');
+        strictEqual(this.model.attributes.text, _.escape('<b>text</b>'));
 
-        this.schema.set('text', 999999.99);
-        strictEqual(this.schema.attributes.text, '999999.99');
+        this.model.set('text', 999999.99);
+        strictEqual(this.model.attributes.text, '999999.99');
 
-        this.schema.set('text', true);
-        strictEqual(this.schema.attributes.text, 'true');
+        this.model.set('text', true);
+        strictEqual(this.model.attributes.text, 'true');
 
-        this.schema.set('text', null);
-        strictEqual(this.schema.attributes.text, null);
+        this.model.set('text', null);
+        strictEqual(this.model.attributes.text, null);
 
-        this.schema.set('text', undefined);
-        strictEqual(this.schema.attributes.text, null);
+        this.model.set('text', undefined);
+        strictEqual(this.model.attributes.text, null);
     });
 
     test('set currency property', function () {
-        this.schema.set('currency', '$999,999.99');
-        strictEqual(this.schema.attributes.currency, 999999.99);
+        this.model.set('currency', '$999,999.99');
+        strictEqual(this.model.attributes.currency, 999999.99);
 
-        this.schema.set('currency', 999999.99);
-        strictEqual(this.schema.attributes.currency, 999999.99);
+        this.model.set('currency', 999999.99);
+        strictEqual(this.model.attributes.currency, 999999.99);
 
-        this.schema.set('currency', true);
-        strictEqual(isNaN(this.schema.attributes.currency), true);
+        this.model.set('currency', true);
+        strictEqual(isNaN(this.model.attributes.currency), true);
 
-        this.schema.set('currency', null);
-        strictEqual(this.schema.attributes.currency, null);
+        this.model.set('currency', null);
+        strictEqual(this.model.attributes.currency, null);
 
-        this.schema.set('currency', undefined);
-        strictEqual(this.schema.attributes.currency, null);
+        this.model.set('currency', undefined);
+        strictEqual(this.model.attributes.currency, null);
     });
 
     test('set percent property', function () {
-        this.schema.set('percent', '99.99 %');
-        strictEqual(this.schema.attributes.percent, 0.9998999999999999);
+        this.model.set('percent', '99.99 %');
+        strictEqual(this.model.attributes.percent, 0.9998999999999999);
 
-        this.schema.set('percent', 999999.99);
-        strictEqual(this.schema.attributes.percent, 999999.99);
+        this.model.set('percent', 999999.99);
+        strictEqual(this.model.attributes.percent, 999999.99);
 
-        this.schema.set('percent', true);
-        strictEqual(isNaN(this.schema.attributes.percent), true);
+        this.model.set('percent', true);
+        strictEqual(isNaN(this.model.attributes.percent), true);
 
-        this.schema.set('percent', null);
-        strictEqual(this.schema.attributes.percent, null);
+        this.model.set('percent', null);
+        strictEqual(this.model.attributes.percent, null);
 
-        this.schema.set('percent', undefined);
-        strictEqual(this.schema.attributes.percent, null);
+        this.model.set('percent', undefined);
+        strictEqual(this.model.attributes.percent, null);
     });
 
     test('add property with options', function () {
-        this.schema.defineProperty('index', 'number', { index: true });
-        strictEqual(this.schema.idAttribute, 'index');
+        this.model.defineProperty('index', 'number', { index: true });
+        strictEqual(this.model.idAttribute, 'index');
 
-        this.schema.defineProperty('default', 'number', { 'default': 0 });
-        strictEqual(this.schema.attributes['default'], 0);
+        this.model.defineProperty('default', 'number', { 'default': 0 });
+        strictEqual(this.model.attributes['default'], 0);
     });
 });
